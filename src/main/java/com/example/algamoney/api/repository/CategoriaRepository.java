43a72bd9d3208b267793cb2b6ab70f48472b2c3a
Anderson Freitas <anderson.souza.freitas@gmail.com>
2022-07-20 19:36:48 -0300

    Desafio: Criando o cadastro de Pessoa implementando o findByNome e findByNomeContaining

--- src/main/java/com/example/algamoney/api/repository/CategoriaRepository.java
+++ src/main/java/com/example/algamoney/api/repository/CategoriaRepository.java
@@ -1,9 +1,14 @@
 package com.example.algamoney.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
+		
+	List<Categoria> findByNome(String nome);
+	List<Categoria> findByNomeContaining(String nome);
+	
 }
